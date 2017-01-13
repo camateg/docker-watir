43240d2d1347326764bedbd21e92292206ed6fa2
Matthew Fleeger <mfleeger5@gmail.com>
2017-01-13 16:14:31 -0500

    Removed unnecessary /util

--- util/install_chrome.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - 
-sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-
-apt-get -y update 
-apt-get -y install google-chrome-stable
